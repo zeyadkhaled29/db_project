7bd71661cc67272b38f3bac1386b0e08d5446c9b
zeyadkhaled29 <zeyadbadry29@gmail.com>
2023-12-22 17:23:29 +0200

    try

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'START'
+    return render_template('main.html')
